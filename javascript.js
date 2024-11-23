125c5cfdc1e29deaaa916228cc04daf1a6dcd0e8
b0811 <typhloziken@gmail.com>
2024-11-23 15:36:11 -0500

    forgot commits add basis functions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,44 @@
-console.log("Hello World")
+// create a function called getComputerChoice that randomly generates and returns "rock", "paper", or "scissors" as a string
+let computerPlayer = getComputerChoice()
+function getComputerChoice(computerPlayer) {
+    let bop = Math.floor(Math.random()* 10) + 1;
+    if (bop <= 4) {
+        return  "rock"
+    } else if (bop > 4 && bop <= 7) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+//write a function called getHumanChoice that will take a users input/choice and returns it as a string.
+function getHumanChoice(choice){
+    let pChoice = prompt("Choose your weapon!: ");
+    pChoice = pChoice.toLocaleLowerCase();
+    if ((pChoice === "scissors") || (pChoice === "paper") || (pChoice === "rock")) {
+        alert("You've chosen: " + pChoice + "!") 
+        return pChoice 
+    } else {
+        alert("Invalid choice")
+    }
+    
+}
+//create a variable to keep track of the players score called humanScore
+let humanScore = 0
+//create a variable to keep track of the computers score called computerScore
+let computerScore = 0
+/*create a function that will play a single round called playRound.
+define two parameters for playRound: humanChoice and computerChoice.
+Use these two parameters to take the human and computer choices as arguments.
+make the humanChoice case-insensitive so they can input any correctly spelled valid choice.
+include a console.log to put a string value representing the winner/loser. example - "you lose, paper beats rock!"
+increment the human or computer score variable based on the winner.*/
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+
+}
+/*the game will be played for 5 rounds. create a function called playGame that calls
+playRound to play 5 rounds, keeps track of the scores, and declares a winner at the end.
+move the playRound function into playGame.*/
